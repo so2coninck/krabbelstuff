747cab3776a78475c39fd4bb5b310d1cc1ed0035
sophie de coninck <scourbey@gmail.com>
2017-01-31 11:25:06 +0100

    correctif4

--- app/models/task.rb
+++ app/models/task.rb
@@ -4,12 +4,7 @@ validates :name, presence: { message:"Merci de completer le nom des parents"}
 validates :child_name, presence: { message:"Merci de completer le prénom de l'enfant"}
 validates :promotion, presence: { message:"Merci de completer l'année"}
 
-
-after_create :send_nvmsg_email
-
 private
-  def send_nvmsg_email
-    UserMailer.nvmsg.deliver_now
-  end
+
 end
 
