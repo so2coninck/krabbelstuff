16d65439df4c99e6dbcc37b2d11e8752d8d82a1d
sophie de coninck <scourbey@gmail.com>
2017-02-01 15:57:29 +0100

    finalisation du livre d'or

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,6 +1,6 @@
 class Task < ApplicationRecord
 
-validates :name, presence: { message:"Merci de completer le nom des parents"}
+validates :name, presence: { message:"Merci de completer votre prénom"}
 validates :child_name, presence: { message:"Merci de completer le prénom de l'enfant"}
 validates :promotion, presence: { message:"Merci de completer l'année"}
 
