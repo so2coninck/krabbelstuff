431ff329d96ec70810bfea56da6a817ca585583f
sophie de coninck <scourbey@gmail.com>
2017-02-20 09:13:41 +0100

    fin du projet- orth

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,8 +1,8 @@
 class Task < ApplicationRecord
 
-validates :name, presence: { message:"Merci de completer votre prénom"}
-validates :child_name, presence: { message:"Merci de completer le prénom de l'enfant"}
-validates :promotion, presence: { message:"Merci de completer l'année"}
+validates :name, presence: { message:"Merci de compléter votre prénom"}
+validates :child_name, presence: { message:"Merci de compléter le prénom de l'enfant"}
+validates :promotion, presence: { message:"Merci de compléter l'année"}
 
 after_save :send_nvmsg_email
 
