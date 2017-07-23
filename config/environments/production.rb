9e7b07e8f25d3933de947944828db123ceccefb7
sophie de coninck <scourbey@gmail.com>
2017-07-23 12:32:38 +0200

    changement @2

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -86,5 +86,5 @@ Rails.application.configure do
 
  config.action_mailer.delivery_method     = :postmark
   config.action_mailer.postmark_settings   = { api_key: ENV['POSTMARK_API_KEY'] }
-  config.action_mailer.default_url_options = { host: "krabbelstuff.herokuapp.com"
+  config.action_mailer.default_url_options = { host: "krabbelstuff.herokuapp.com" }
 end
