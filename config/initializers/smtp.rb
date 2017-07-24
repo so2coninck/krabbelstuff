7f1c6d905a31bd18773197bfb2aa6544474a1f1d
sophie de coninck <scourbey@gmail.com>
2017-07-24 20:16:37 +0200

    changement @3

--- config/initializers/smtp.rb
+++ config/initializers/smtp.rb
@@ -1,3 +1,9 @@
 ActionMailer::Base.smtp_settings = {
-
+ address: "smtp.postmarkapp.compile",
+  port: '25',
+  domain: 'heroku.com',
+  user_name: ENV['POSTMARK_API_TOKEN'],
+  password: ENV['POSTMARK_API_TOKEN'],
+  authentication: :login,
+  enable_starttls_auto: true
 }
